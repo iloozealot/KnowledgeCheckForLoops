28d4c9db95e3a67e51e2c0043ef8e76ca56e6fed
iloozealot <iloozealot@gmail.com>
2023-11-16 20:21:55 -0600

    Add project files.

---
+++ AssignmentForLoops/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//Console application?
+//Ask User for comma-separated list of first names?
+//Split string into string array?
+// Loop through array and print "Hello <name>" to the console for each person?
+
+string? input = null;
+
+Console.Write("Please enter comma-separated list of first names (no spaces). ");
+input = Console.ReadLine();
+
+string[] firstNames = input.Split(",").ToArray();
+
+for (int i = 0; i < firstNames.Length; i++)
+{
+    Console.WriteLine($"Hello {firstNames[i]}");
+}
+
+
